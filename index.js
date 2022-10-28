59837e51c235ae4dcf53021c7c704efd0abe094b
Ali Adeku <aliadeku.aam@gmail.com>
2022-10-28 18:05:30 +0100

    Connect to database from app

--- index.js
+++ index.js
@@ -14,6 +14,17 @@ app.get('/',(req,res)=>{
     })
 })
 
+mongoose.connect("mongodb://localhost:27017/blog");
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB Successfully");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("An error occurred while connecting to MongoDB");
+  console.log(err);
+});
+
 app.listen(PORT ,()=>{
     console.log(`listen on port ${PORT}`)
 })
