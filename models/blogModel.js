7d51648b195b33763bf7a1b20e98e70825088a8c
Ali Adeku <aliadeku.aam@gmail.com>
2022-11-01 19:08:51 +0100

    Create BlogPost model

--- models/blogModel.js
+++ models/blogModel.js
@@ -3,29 +3,52 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
-const BlogSchema = new Schema({
+const BlogPostSchema = new Schema({
     id:ObjectId,
     created_at : Date,
 
     title :{
      type:String,
-     required:[true,'title must be provided']
+     required:[true,'title must be provided'],
+     unique:true
     },
 
     description:{
     type:String,
-    required:[true,'description must be provided']
     },
 
-    tags:{
-     type:String,
-     enum:['sport','entertainment','fashion']
+    body:{
+        type:String,
+     required:[true,'body must be provided']
     },
+
+    tags:[String],
 
     author:{
         type:String,
-        required:[true,'name of author is required']
-    }
+    },
+
+    state:{
+       type:String,
+       enum:['draft','published']
+    },
+
+   read_count:{
+    type:Number
+   },
+
+   reading_time:{
+    type:Number
+   },
+
+    createAt: {
+        type: Date,
+        default: Date.now(),
+      },
 
     
 })
+
+const BlogPost = mongoose.model("BlogPost",BlogPostSchema);
+
+module.exports = BlogPost
