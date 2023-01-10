f23d759b8eeeb3531f353d92b821cb4182521a68
Ali Adeku <aliadeku.aam@gmail.com>
2023-01-10 16:56:19 +0100

    Add swagger docs

--- app.js
+++ app.js
@@ -9,7 +9,12 @@ const errorHandler = require("./src/middlewares/error-handler");
 require("./src/middlewares/auth"); //signup and login middleware
 
 const app = express();
+
 const cors = require("cors")
+//swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load("./swagger.yaml")
 
 
 // middlewares
@@ -17,6 +22,12 @@ app.use(express.json());
 app.use(cors())
 
 app.use(httpLogger);
+
+app.get("/",(req,res)=>{
+    res.send('<h1>Blog API</h1><a href="/api-docs">documentation</a>')
+})
+
+app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerDocument))
 
 app.use(authRoutes);
 app.use(blogRoutes);
